ee50603d635befa4b16ff15db2f017adc91c7f2c
o.lucaxx <lucas.s.pereira579@gmail.com>
2024-10-07 13:18:06 -0300

    Atualização em Main.java

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Esse é meu primeiro repositório do GitHub!
 
 Agora estou atualizando meu README pelo próprio editor de texto do GitHub.
 Estou aprendendo muito com o w3schools!
+
+Agora nosso Main.java imprime a soma de 1 com 2!
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+        public static void main(String[] args) {
+                int firstNum = 1;
+                int secondNum = 2;
+                int sum = firstNum + secondNum;
+                System.out.println(String.format("A soma de %d com %d é %d", firstNum, secondNum, sum));
+        }
 }
+

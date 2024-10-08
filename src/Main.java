816479002f28fafe5488ef71f4935ac5c4d2c4fd
Lucas Pereira <122061240+olucaxx@users.noreply.github.com>
2024-10-08 16:41:16 -0300

    update Main.java
    agora o main.java vai exibir apenas "Olá mundo!"

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
         public static void main(String[] args) {
-                int firstNum = 1;
-                int secondNum = 2;
-                int sum = firstNum + secondNum;
-                System.out.println(String.format("A soma de %d com %d é %d", firstNum, secondNum, sum));
+                System.out.println("Olá mundo!");
         }
 }
 
